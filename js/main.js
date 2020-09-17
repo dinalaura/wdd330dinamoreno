a870fab087990e0bc32ed6dbd4a7e32264157619
dinalaura <64177544+dinalaura@users.noreply.github.com>
2020-09-17 15:10:12 -0500

    commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const links = [{
+    label: "Week1 notes",
+    url: "week1/index.html"
+  },
+  {
+    label: "Week2 notes",
+    url: "week2/index.html"
+  }
+]
+
+const myList = document.querySelector("ol");
+
+links.forEach(links => {
+  let item = document.createElement("li");
+  let anchor = document.createElement("a");
+  anchor.setAttribute("href", links.url);
+  anchor.innerText = links.label;
+  
+  item.appendChild(anchor);
+  myList.appendChild(item); 
+});
