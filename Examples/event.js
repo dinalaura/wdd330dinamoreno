6454eb9d6f5c56a248ce8eb5f18f4a4d3b9f4f1b
dinalaura <dinaguev1@gmail.com>
2020-10-09 16:17:04 -0500

    commit

--- Examples/event.js
+++ Examples/event.js
@@ -1,5 +1,52 @@
 
 
-document.getElementById("div").addEventListener.function myFunction() {
-  document.querySelector("div").innerHTML = "O";
+function myFunction() {
+    document.querySelector("uno").innerHTML = "O";
+  }
+
+
+function myFunction() {
+  document.querySelector("dos").innerHTML = "O";
+}
+
+
+
+function myFunction() {
+  document.querySelector("tres").innerHTML = "O";
+}
+
+
+
+function myFunction() {
+  document.querySelector("cuatro").innerHTML = "O";
+}
+
+
+
+function myFunction() {
+  document.querySelector("cinco").innerHTML = "O";
+}
+
+
+
+function myFunction() {
+  document.querySelector("seis").innerHTML = "O";
+}
+
+
+
+function myFunction() {
+  document.querySelector("siete").innerHTML = "O";
+}
+
+
+
+function myFunction() {
+  document.querySelector("ocho").innerHTML = "O";
+}
+
+
+
+function myFunction() {
+  document.querySelector("nueve").innerHTML = "O";
 }
