ca4ba01ec7f83b8751b8dbac8ce1c26b861ed2cf
dinalaura <dinaguev1@gmail.com>
2020-11-21 22:16:02 -0600

    commit

---
+++ week10/earthquake app/index.js
@@ -0,0 +1,6 @@
+
+import QuakesController from './QuakesController.js';
+import buildNavigation from './routing.js';
+
+const navElement = document.getElementById('mainNav');
+buildNavigation(navElement);
